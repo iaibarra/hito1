2816af14e01834e2367fcac6a9e2a0b861de3562
Iris <iris@Mac-mini-de-Iris.local>
2025-06-01 20:59:41 -0400

    Primer commit - Proyecto Pizzería Mamma Mía

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="footer text-light text-center p-3 mt-4">
+    © 2021 - Pizzería Mamma Mía! - Todos los derechos reservados
+  </footer>
+  )
+}
+
+export default Footer
