5d5c67966a5f768eb33e88ac37aae1a1e7f58ab0
Iris <iris@SENADORs-MacBook-Pro.local>
2025-06-07 15:06:06 -0400

    feat: subir avances del hito 2 con estilos y validaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
 import Footer from './components/Footer'
 import Home from './components/Home'
+import Login from './components/Login'
 import Navbar from './components/Navbar'
+import Register from './components/Register'
 
 function App() {
 
   return (
     <>
       <Navbar/>
-      <Home/>
+     {/* <Home/> */}
+     <Register/> 
+      <Login/>
       <Footer/>
     </>
   )
---
+++ src/components/Register.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+
+const Register = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [checkPassword, setcheckPassword] = useState("")
+
+    const validarDatos = (e) => {
+        e.preventDefault()
+        if (!email.trim() || !password.trim() || !checkPassword.trim()){
+            alert("Todos los campos son obligatorios")
+        }else if (password.length < 6) {
+            alert("La contraseña debe tener al menos 6 caracteres");
+        }else if (password !== checkPassword) {
+                alert("Las contraseñas deben coincidir");
+        }else {
+            alert(`Bienvenido/a ${email}`)
+          }
+    }
+  return (
+    <section>
+        <div>
+            <h2>Registrarse</h2>
+        </div>
+    <form onSubmit={validarDatos}>
+      <div>
+          <label>Email</label>
+          <input type='email' onChange={(e) => ( setEmail(e.target.value))}></input>
+      </div>
+      <div>
+          <label>Password</label>
+          <input type='password' onChange={(e) => ( setPassword(e.target.value))}></input>
+      </div>
+      <div>
+          <label>Check Password</label>
+          <input type='password' onChange={(e) => ( setcheckPassword(e.target.value))}></input>
+      </div>
+      <div>
+          <button type='submit'>Registrarse</button>
+      </div>
+      <div>
+          <p>Datos agregados:</p>
+          <p>Email: {email}</p>
+      </div>
+    </form>
+  </section>
+  )
+}
+
+export default Register
